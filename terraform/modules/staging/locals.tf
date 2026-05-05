e71e9768bc1405535cd6060a0db7f9cbcf947803
robot0001 <urban.peta@gmail.com>
2026-05-05 15:11:55 +0200

    fix: remove unused rds_security_group_id local

--- terraform/modules/staging/locals.tf
+++ terraform/modules/staging/locals.tf
@@ -1,7 +1,6 @@
 locals {
   vpc_id                = data.terraform_remote_state.platform.outputs.vpc_id
   private_subnet_ids    = data.terraform_remote_state.platform.outputs.private_subnet_ids
-  rds_security_group_id = data.terraform_remote_state.platform.outputs.rds_security_group_id
   rds_master_secret_arn = data.terraform_remote_state.platform.outputs.rds_master_secret_arn
   rds_host              = data.terraform_remote_state.platform.outputs.rds_endpoint
   rds_port              = data.terraform_remote_state.platform.outputs.rds_port
